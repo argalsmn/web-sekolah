6a75b21c6b2f2455c028a6ad773609aaf32e69e2
Argaaa63 <argaaaa53@gmail.com>
2024-12-28 07:04:18 +0700

    view testing n use template

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\mainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::prefix('/')->group(function(){
+    Route::get('/',[mainController::class,'index'])->name('index');
 });
